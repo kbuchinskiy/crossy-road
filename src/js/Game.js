94674914fd26bb5074eefcdf8da5b028064db3bd
kbuchinskiy <kirillsmailbox@gmail.com>
2019-04-07 22:32:11 +0300

    update(Game): hurdlre generating, hurdle and hurdle items hit handlers, attempts

--- src/js/Game.js
+++ src/js/Game.js
@@ -8,17 +8,21 @@ import playerImage from "../images/cat.png";
 import roadBg from "../images/road.jpg";
 import riverBg from "../images/water.jpg";
 import treeFieldBg from "../images/grass.jpg";
+import finishLineImage from "../images/finishLine.jpg";
 import carImage from "../images/car.png";
 import raftImage from "../images/raft.jpg";
 import treeImage from "../images/tree.png";
+import heartImage from "../images/heart.png";
 
 export default class Game {
   constructor(config) {
     this.setup = this.setup.bind(this);
     this.config = config;
     this.config.hurdles.reverse();
-
+    this.attemptsAmount = config.attempts;
     this.hurdles = [];
+    this.finishLine;
+    this.attemptsBar;
 
     this.app = new PIXI.Application({
       width: 600,
@@ -36,16 +40,20 @@ export default class Game {
         roadBg,
         riverBg,
         treeFieldBg,
+        finishLineImage,
         carImage,
         raftImage,
-        treeImage
+        treeImage,
+        heartImage
       ])
       .load(this.setup);
   }
 
   setup() {
+    this.addFinishLine();
+    this.addAttemptsBar();
     this.genereteHurdles();
-    this.addPlayer(Player);
+    this.addPlayer();
 
     this.state = this.play;
     this.app.ticker.add(delta => this.gameLoop(delta));
@@ -55,23 +63,97 @@ export default class Game {
     this.state(delta);
   }
 
-  play(delta) {
-    this.updatePlayer();
-    this.disablePlayerDisappearance();
-    this.updateDynamicObstalces();
+  removeAttempt() {
+    let attempts = this.attemptsBar.children;
+    attempts[attempts.length - 1].destroy();
+  }
 
-    this.hurdles.forEach(hurdle => {
-      if (hitTestRectangle(this.playerSprite, hurdle.container)) {
-        this.setPlayerInitialProps();
+  play(delta) {
+    if (this.attemptsAmount === 0) {
+      this.app.ticker.stop();
+      alert("You lost");
+      location.reload();
+    } else {
+      this.updatePlayer();
+      this.disablePlayerDisappearance();
+      this.updateDynamicObstalces();
+
+      if (hitTestRectangle(this.playerSprite, this.finishLine)) {
+        alert("You win");
+        this.app.ticker.stop();
+        location.reload();
+      }
+
+      this.hurdles.forEach(hurdle => {
+        if (hitTestRectangle(this.playerSprite, hurdle.container)) {
+          this.hurdleHitHandler(hurdle);
+        }
+      });
+    }
+  }
+
+  hurdleHitHandler(hurdle) {
+    if (hurdle instanceof StaticHurdle) {
+      this.staticItemHitHandler(hurdle);
+    } else {
+      this.dynamicItemHitHandler(hurdle);
+    }
+  }
+
+  staticItemHitHandler(hurdle) {
+    hurdle.items.forEach(item => {
+      if (this.playerSprite.y === hurdle.container.y) {
+        if (hitTestRectangle(this.playerSprite, item, true)) {
+        }
       }
     });
   }
+
+  dynamicItemHitHandler(hurdle) {
+    if (hurdle.type === "road") {
+      hurdle.items.forEach(item => {
+        if (hitTestRectangle(this.playerSprite, item, true)) {
+          this.attemptsAmount--;
+          this.removeAttempt();
+          this.setPlayerInitialProps();
+        }
+      });
+    } else if (hurdle.type === "river") {
+      hurdle.items.forEach(item => {
+        if (hitTestRectangle(this.playerSprite, item, true)) {
+          this.playerSprite.x =
+            item.x + item.width / 2 - this.playerSprite.width / 2;
+        }
+      });
+    }
+  }
+
   addPlayer() {
     this.player = new Player(playerImage, 5);
     this.playerSprite = this.player.sprite;
     this.setPlayerInitialProps();
     this.app.stage.addChild(this.playerSprite);
   }
+
+  addFinishLine() {
+    this.finishLine = new PIXI.Sprite(
+      PIXI.loader.resources[finishLineImage].texture
+    );
+    this.finishLine.y = 16;
+    this.app.stage.addChild(this.finishLine);
+  }
+
+  addAttemptsBar() {
+    this.attemptsBar = new PIXI.Container();
+    for (let i = 0; i < this.config.attempts; i++) {
+      let attempt = new PIXI.Sprite(PIXI.loader.resources[heartImage].texture);
+      attempt.x += i * attempt.width;
+      this.attemptsBar.addChild(attempt);
+    }
+
+    this.app.stage.addChild(this.attemptsBar);
+  }
+
   updatePlayer() {
     this.playerSprite.x += this.playerSprite.vx;
     this.playerSprite.y += this.playerSprite.vy;
@@ -79,24 +161,34 @@ export default class Game {
 
   updateDynamicObstalces() {
     this.hurdles.forEach(hurdle => {
-      if (!hurdle.static) {
+      if (hurdle instanceof DynamicHurdle) {
         hurdle.update();
       }
     });
   }
 
-  setPlayerInitialProps() {
-    this.playerSprite.width = 32;
-    this.playerSprite.height = 32;
+  genereteHurdles() {
+    const hurdlesList = this.config.hurdles;
 
-    const playerY = this.app.renderer.view.height - this.playerSprite.height;
-    const playerX = this.app.renderer.view.width / 2;
+    hurdlesList.forEach((hurdleType, index) => {
+      let hurdle;
+      switch (hurdleType) {
+        case "road":
+          hurdle = new DynamicHurdle(roadBg, carImage);
+          break;
+        case "river":
+          hurdle = new DynamicHurdle(riverBg, raftImage);
+          break;
+        case "treeField":
+          hurdle = new StaticHurdle(treeFieldBg, treeImage);
+          break;
+      }
+      hurdle.type = hurdleType;
+      this.hurdles.push(hurdle);
 
-    this.playerSprite.position.set(playerX, playerY);
-    this.playerSprite.position.set(playerX, playerY);
-
-    this.playerSprite.vx = 0;
-    this.playerSprite.vy = 0;
+      hurdle.container.y = index * 32 + 48;
+      this.app.stage.addChild(hurdle.container);
+    });
   }
 
   disablePlayerDisappearance() {
@@ -125,28 +217,17 @@ export default class Game {
     }
   }
 
-  genereteHurdles() {
-    const hurdlesList = this.config.hurdles;
+  setPlayerInitialProps() {
+    this.playerSprite.width = 32;
+    this.playerSprite.height = 32;
 
-    hurdlesList.forEach((hurdleType, index) => {
-      let hurdle;
-      switch (hurdleType) {
-        case "road":
-          hurdle = new DynamicHurdle(roadBg, carImage);
-          break;
-        case "river":
-          hurdle = new DynamicHurdle(riverBg, raftImage);
-          break;
-        case "treeField":
-          hurdle = new StaticHurdle(treeFieldBg, treeImage);
-          hurdle.static = true;
-          break;
-      }
-      hurdle.type = hurdleType;
-      this.hurdles.push(hurdle);
+    const playerY = this.app.renderer.view.height - this.playerSprite.height;
+    const playerX = this.app.renderer.view.width / 2;
 
-      hurdle.container.y = index * 32 + 32;
-      this.app.stage.addChild(hurdle.container);
-    });
+    this.playerSprite.position.set(playerX, playerY);
+    this.playerSprite.position.set(playerX, playerY);
+
+    this.playerSprite.vx = 0;
+    this.playerSprite.vy = 0;
   }
 }
