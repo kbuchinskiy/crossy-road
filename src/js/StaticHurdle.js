4b017c67aa5e46ff3438f4485d4ccb8af77cb20f
kbuchinskiy <kirillsmailbox@gmail.com>
2019-04-08 10:58:26 +0300

    update(StaticHurdle)

--- src/js/StaticHurdle.js
+++ src/js/StaticHurdle.js
@@ -4,16 +4,18 @@ import { getRandomInt, hitTestRectangle } from "./utils.js";
 export default class StaticHurdle {
   constructor(backgroundImage, staticItemImage) {
     this.container = new PIXI.Container();
+    
     const background = new PIXI.Sprite(
       PIXI.loader.resources[backgroundImage].texture
     );
 
     this.container.addChild(background);
     this.items = [];
-    this.initStaticItems(staticItemImage);
+    this.backgroundTexture = background;
+    this._initStaticItems(staticItemImage);
   }
 
-  initStaticItems(staticItemImage) {
+  _initStaticItems(staticItemImage) {
     const itemsAmount = getRandomInt(3, 6);
 
     for (let i = 0; i <= itemsAmount; i++) {
@@ -22,7 +24,7 @@ export default class StaticHurdle {
       );
 
       staticItem.x = getRandomInt(i * staticItem.width, 600 - staticItem.width);
-      
+
       if (!this.items.some(item => hitTestRectangle(staticItem, item, true))) {
         this.items.push(staticItem);
         this.container.addChild(staticItem);
