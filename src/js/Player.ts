6e5a4e936411279d27e540dac224bffc77072c3f
kbuchinskiy <kirillsmailbox@gmail.com>
2019-04-12 21:18:32 +0300

    update(Player, Game)

--- src/js/Player.ts
+++ src/js/Player.ts
@@ -2,8 +2,8 @@ export class Player extends PIXI.Sprite {
     direction: number = 0;
     speed: number = 0;
 
-    constructor(height: number, width: number, texture: PIXI.Texture) {
-        super(texture);
+    constructor(height: number, width: number, playerImage: string) {
+        super(PIXI.Texture.fromImage(playerImage));
 
         this.height = height;
         this.width = width;
