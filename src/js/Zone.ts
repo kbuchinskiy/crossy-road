ee268377f7ee06cc5302e605f0f5db81a98c571f
kbuchinskiy <kirillsmailbox@gmail.com>
2019-04-13 15:33:06 +0300

    update(Zone): abstract

--- src/js/Zone.ts
+++ src/js/Zone.ts
@@ -1,14 +1,15 @@
 
 import { getRandomInt, hitTestRectangle } from './utils'
 
-export class Zone extends PIXI.Container {
+export abstract class Zone extends PIXI.Container {
+
     private readonly background: PIXI.Sprite;
-    readonly itemsAmount: number;
     itemsContainer: PIXI.Container = new PIXI.Container;
 
     constructor(backgroundImage: string, readonly isSafe) {
         super();
         this.background = PIXI.Sprite.fromImage(backgroundImage);
+    
         this.addChild(this.background);
         this.addChild(this.itemsContainer);
     }
