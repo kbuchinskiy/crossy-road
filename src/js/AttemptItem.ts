1570045b2cf542b8d1bb4dc3fb8be9d78d4b218c
kbuchinskiy <kirillsmailbox@gmail.com>
2019-04-14 15:47:23 +0300

    add(AttemptBar, FinishLine)

---
+++ src/js/AttemptItem.ts
@@ -0,0 +1,7 @@
+import { SpriteProps, SpriteItem } from './SpriteItem';
+
+export default class AttemptItem extends SpriteItem {
+  constructor(props: SpriteProps) {
+    super(props);
+  }
+}
---
+++ src/js/AttemptsBar.ts
@@ -0,0 +1,34 @@
+
+import AttemptItem from './AttemptItem';
+
+interface iAttemptsBar {
+  readonly attemptsAmount: number,
+  removeAttempt()
+}
+
+export default class AttemptsBar extends PIXI.Container implements iAttemptsBar {
+  private _attemptsLeft: number;
+  constructor(readonly attemptsAmount: number, private readonly attemptIcon: string, private readonly spaceBetween: number) {
+    super();
+    this.mountAttempts();
+    this._attemptsLeft = attemptsAmount;
+  }
+
+  private mountAttempts() {
+    for (let i = 0; i < this.attemptsAmount; i++) {
+      const attemptItem = new AttemptItem({ image: this.attemptIcon, width: 10, height: 10 });
+      attemptItem.x = i * this.spaceBetween;
+      this.addChild(attemptItem);
+    }
+  }
+
+  get attemptsAvailable(): number {
+    return this._attemptsLeft;
+  }
+
+
+  removeAttempt() {
+    this.children.pop();
+    this._attemptsLeft--;
+  }
+}
