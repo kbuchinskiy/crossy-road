e7a06583b5bd1d4aa26501cae39ef533110e0baa
kbuchinskiy <kirillsmailbox@gmail.com>
2019-05-06 10:42:32 +0300

    update(ZoneDynamic.ts): refactoring

--- src/js/ZoneDynamic.ts
+++ src/js/ZoneDynamic.ts
@@ -6,40 +6,51 @@ import { SpriteItem, SpriteProps } from './SpriteItem';
 interface iZoneDynamic {
   readonly direction: number;
   readonly speed: number;
+  stopItemsBuilder(): void;
   updateItems(): void
 }
 
 export default class ZoneDynamic extends Zone implements iZoneDynamic {
+  private itemsBuilderIntervalId: number;
   readonly direction: number = Math.random() < 0.5 ? -1 : 1;
 
   constructor(backgroundImage: string, zoneWidth: number, zoneHeight: number, readonly isSafe, readonly speed: number, spriteSettings: SpriteProps) {
     super(backgroundImage, zoneWidth, zoneHeight, spriteSettings);
 
+    this.mountItem();
     this.itemsBuilder();
     this.updateItems();
   }
 
+  private mountItem(): void {
+    const item = new SpriteItem(this.itemConfig);
+
+    if (this.direction > 0) {
+      item.x = -item.width -
+        getRandomInt(
+          item.width,
+          item.width * 2
+        );
+    } else {
+      item.x = 400 + item.width * 4;
+      item.scale.x = -1;
+      item.anchor.x = 1;
+    }
+
+    this.itemsContainer.addChild(item);
+
+  }
+
   private itemsBuilder(): void {
-
-    setInterval(() => {
-      setTimeout(() => {
-        const item = new SpriteItem(this.itemConfig);
-
-        if (this.direction > 0) {
-          item.x = -item.width -
-            getRandomInt(
-              item.width,
-              item.width * 2
-            );
-        } else {
-          item.x = 400 + item.width * 4;
-          item.scale.x = -1;
-          item.anchor.x = 1;
-        }
-        this.itemsContainer.addChild(item);
+    this.itemsBuilderIntervalId = window.setInterval(() => {
+      window.setTimeout(() => {
+        this.mountItem();
       }, getRandomInt(1, 3) * 1000);
-
     }, 3000);
+  }
+
+  stopItemsBuilder() {
+    clearInterval(this.itemsBuilderIntervalId);
   }
 
   updateItems(): void {
